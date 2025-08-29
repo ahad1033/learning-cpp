29f0decc4d951002f2fe8a0b996e051fd5500183
ahad1033 <a.ahad2232@gmail.com>
2025-08-30 02:53:08 +0600

    Data types in cpp

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int score;   // DECLARE
+    score = 110; // INITIALIZE
+
+    int maxScore = 200; // DEC + INI
+
+    const int UID = 234585; // CAN NOT REASSIGN
+
+    char userName = 'ahad';
+
+    cout << "Welcome to var const" << endl;
+    return 0;
+}
---
+++ 02_primitives/data.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int currentBalance = 50;
+    float salary = 100.25;
+    double dinnerBill = 50.46;
+    char alpha = 'A';
+    bool isVerified = true;
+    long largeStorage = 100000;
+    short shortStorage = 10;
+
+    // cout << largeStorage << endl;
+    return 0;
+}

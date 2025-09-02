15758cb6d5ce2f1f5b7cdad92eaf69fadc76dd1d
ahad1033 <a.ahad2232@gmail.com>
2025-09-02 17:36:36 +0600

    Problem solving

--- 00_problem_solving/main.cpp
+++ 00_problem_solving/main.cpp
@@ -16,6 +16,8 @@ int main()
     }
     */
 
+    /*
+    // Take an integer N from the user and print the sum of numbers from 1 to N.
     int num;
     int total = 0;
 
@@ -28,6 +30,33 @@ int main()
     }
 
     cout << "Total is: " << total << endl;
+    */
+
+    /*
+    // Take a number from the user and print its multiplication table (up to 10).
+    int num;
+
+    cout << "Enter a number: " << endl;
+    cin >> num;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << num << " * " << i << " = " << num * i << endl;
+    }
+    */
+
+    // Print numbers from N down to 1 using a loop.
+    int num;
+
+    cout << "Please enter a number: " << endl;
+    cin >> num;
+
+    while (num >= 1)
+    {
+        cout << num << endl;
+
+        num--;
+    }
 
     return 0;
 }

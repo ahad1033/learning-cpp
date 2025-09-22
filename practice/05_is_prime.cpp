7b2114b1e3631b7d0a5d5138d745f70b9e63b712
ahad1033 <a.ahad2232@gmail.com>
2025-09-23 01:32:22 +0600

    Findin prime number

---
+++ practice/05_is_prime.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    bool isPrime = true;
+
+    cout << "Enter a number to check if it's a prime number: " << endl;
+    cin >> num;
+
+    if (num <= 1)
+    {
+        isPrime = false;
+    }
+    else
+    {
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+    {
+        cout << num << " is a prime number." << endl;
+    }
+    else
+    {
+        cout << num << " is not a prime number." << endl;
+    }
+
+    return 0;
+}

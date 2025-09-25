edbc7bdc3107e66b592e54cd28e7f0cec890737d
ahad1033 <a.ahad2232@gmail.com>
2025-09-25 21:36:43 +0600

    Floyd's Triangle

--- practice/07_triangle_pattern.cpp
+++ practice/07_triangle_pattern.cpp
@@ -61,5 +61,54 @@ int main()
     //     cout << endl;
     // }
 
+    /*
+        REVERSE TRIANGLE
+    */
+    // int n = 5;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j > 0; j--)
+    //     {
+
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+        FLOYD'S TRIANGLE
+    */
+    // int n = 5;
+
+    // int num = 1;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j > 0; j--)
+    //     {
+
+    //         cout << num << " ";
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+        FLOYD'S TRIANGLE [ ALPHABET ]
+    */
+    // int n = 5;
+
+    // char ch = 'A';
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j > 0; j--)
+    //     {
+
+    //         cout << ch << " ";
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+
     return 0;
 }
